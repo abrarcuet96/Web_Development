b303ab3a776c58f6f2815599dab10fafb0becb10
abrarcuet96 <133400726+abrarcuet96@users.noreply.github.com>
2023-09-07 00:31:59 +0600

    assignment quite done

---
+++ Ass-6/blog.js
@@ -0,0 +1,3 @@
+document.getElementById('blog-view').addEventListener('click', function(){
+    window.location.href='blog.html';
+})
--- Ass-6/index.js
+++ Ass-6/index.js
@@ -3,9 +3,11 @@ const selectCategory = async () => {
     const categoryToJson = await response.json();
     const categories = categoryToJson.data;
     categoryTitle(categories);
+    const cardId = categories.category_id;
+    selectCategoryCards(cardId);
 }
+
 const categoryTitle = (categories) => {
-    // console.log(categories);
     categories.forEach((category) => {
         const categoryItems = document.getElementById('category-title');
         const div = document.createElement('div');
@@ -16,38 +18,50 @@ const categoryTitle = (categories) => {
         categoryItems.appendChild(div);
     });
 }
-const selectCategoryCards = async (id) => {
+const selectCategoryCards = async (id = "1000") => {
+    // console.log(id);
     const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${id}`);
     const categoryCardToJson = await response.json();
     const categoryItems = categoryCardToJson.data;
-    sortClick(categoryItems);
+    cardClick(categoryItems);
+    sortView(id);
 }
-const sortClick = (categories, isTrue) => {
-    if (isTrue) {
-        let newCategory = [];
-        let newIndex = [];
-        for (const category of categories) {
-            let cat = parseFloat(category.others.views);
-            let index = categories.indexOf(category);
-            newCategory.push({ cat, index });
-            newIndex.push({ index });
-        }
-        const sortedProducts = sortArrayOfObjects(newCategory, 'cat', 'descending');
-        let newCategories = [];
-        for (const i of sortedProducts) {
-            newCategories.push(categories[i.index]);
-        }
-        categories = newCategories;
-        cardClick(categories);
+const sortView = (id) => {
+    console.log(id);
+    console.log(id.category_id);
+    const categoryItems = document.getElementById('click-to-sort');
+    categoryItems.textContent = '';
+    const div = document.createElement('div');
+    categoryItems.classList = `text-center my-8 flex justify-center gap-5`;
+    div.innerHTML = `
+    <a onclick="sortClick('${id}')" class="btn btn-ghost normal-case text-xl bg-slate-300">Sort by view</a>
+    `;
+    categoryItems.appendChild(div);
+}
+const sortClick = async (id) => {
+    const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${id}`);
+    const categoryCardToJson = await response.json();
+    let categories = categoryCardToJson.data;
+    let newCategory = [];
+    let newIndex = [];
+    for (const category of categories) {
+        let cat = parseFloat(category.others.views);
+        let index = categories.indexOf(category);
+        newCategory.push({ cat, index });
+        newIndex.push({ index });
     }
-    else {
-        cardClick(categories);
+    const sortedProducts = sortArrayOfObjects(newCategory, 'cat', 'descending');
+    let newCategories = [];
+    for (const i of sortedProducts) {
+        newCategories.push(categories[i.index]);
     }
+    categories = newCategories;
+    cardClick(categories);
+
 }
 const cardClick = (categories) => {
     const categoryCards = document.getElementById('category_cards');
     categoryCards.textContent = '';
-    categoryCards.classList = '';
     if (categories.length == 0) {
         const div = document.createElement('div');
         div.innerHTML = `  
@@ -62,12 +76,18 @@ const cardClick = (categories) => {
     }
     else {
         categories.forEach((category) => {
-            // console.log(category);
-
+            const timeInSeconds = category.others.posted_date;
+            const hours = parseInt(timeInSeconds / (24 * 360));
+            const min = parseInt((parseFloat((timeInSeconds / (24 * 360)) - hours).toFixed(2)) * 60);
             const div = document.createElement('div');
             categoryCards.classList = `grid grid-cols-4 gap-10`;
             div.innerHTML = `<div class="w-[312px] h-96 card">
+                    <div class="relative">
                     <img class="w-[312px] h-[200px] rounded-lg" src="${category.thumbnail}" alt="Shoes" />
+                    <div id="time-duration" class="${hours==0 && min==0?"hidden":""}" >
+                    <p class="text-center text-white w-[130px] h-7 rounded-lg bg-black absolute bottom-2 right-2">${hours} hr ${min} min</p>
+                    </div>
+                    </div>
                     <div class="card-body flex flex-row">
                         <div class="">
                             <img class="w-14 h-14 rounded-full" src="${category.authors[0].profile_picture}" alt="">
@@ -76,20 +96,15 @@ const cardClick = (categories) => {
                             <h2 class="card-title font-bold">${category.title}</h2>
                             <div class="flex">
                                 <p>${category.authors[0].profile_name}</p>
-                            </div>
+                            <div>
+                            </div>${category.authors[0].verified?`<i class="fa-solid fa-certificate"></i>`:''} </div>
                             <p>${category.others.views} views</p>
                         </div>
                     </div>
                 </div>`;
             categoryCards.appendChild(div);
-
         });
     }
 }
-const isSort = (isTrue) => {
-    sortClick(isTrue);
-}
-const clickedSort = () => {
-    isSort(true);
-}
+
 selectCategory();
